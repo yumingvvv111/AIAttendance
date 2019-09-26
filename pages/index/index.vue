78dd85fb9fadc8c28c80e00735c32e968d094ee7
yumingvvv111 <257165528@qq.com>
2019-09-26 17:55:19 +0800

    添加打卡记录当天信息

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -33,6 +33,17 @@
 						</button>
 					</view>
 				</view>
+				
+				<view class="cu-bar bg-white solid-bottom" v-for="(item, index) in punchList" :key="index">
+					<view class="action" style="color:#ccc">
+						<text class="text-orange"><span></span></text>{{item.type}}{{item.time}}
+					</view>
+					<view class="action">
+						<button class="cu-btn" style="background: transparent;">
+							<text><span></span></text>{{item.result}}
+						</button>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -43,6 +54,13 @@
 		data() {
 			return {
 				title: 'map',
+				punchList: [
+					// {
+					// type: '上班',
+					// time: '08:13',
+					// result: '正常',
+					// }
+				],
 				latitude: 40.0418585238,
 				longitude: 116.2998747826,
 				covers: [{
@@ -71,7 +89,44 @@
 				}
 			}
 		},
+		mounted(){
+			uni.getStorage({
+				key: 'punchStatus',
+				success: (res) => {
+					let data = res.data;
+					console.log('get punchStatus success');
+					console.log(data);
+					if(this.checkIsToday(data.date)){
+						uni.getStorage({
+							key: 'punchList',
+							success: (res) => {
+								console.log('get punchList success');
+								console.log(res);
+								this.punchList = res.data;
+							}
+						});
+					}else{
+						uni.setStorage({
+							key: 'punchList',
+							data: [],
+							success: (res) => {
+								
+							}
+						});
+					}
+				}
+			});
+		},
+		
 		methods: {
+			checkIsToday(date){
+				var time = (new Date(date)).getTime();
+				var now = new Date();
+				now = (now.toISOString()).match(/\d{4}-\d{2}-\d{2}/)[0];
+				now = new Date(now);
+				now = now.getTime();
+				return now <= time + 24 * 60 * 60 * 1000;
+			},
 			punch(type) {
 				var alert = this.$util.alert;
 				let date = new Date();
@@ -90,11 +145,18 @@
 				];
 
 				let s = type === 'signIn' ? '到' : '退';
+				let _type = type === 'signIn' ? '上班' : '下班';
 				let preText = `签${s}成功, `;
+				let result = {
+					type: _type,
+					time: (new Date(time)).toString().match(/\d{2}:\d{2}:\d{2}/)[0],
+					result: '异常'
+				};
 				
 				if (!checkHasPunched(type)) {
 					if (type === 'signIn') {
 						if (time <= checkPoint[0]) {
+							result.result = '正常';
 							alert(preText + '考勤正常');
 						} else if (time > checkPoint[0] && time <= checkPoint[1]) {
 							alert(preText + '您已迟到');
@@ -103,11 +165,14 @@
 						}
 					} else {
 						if (time >= checkPoint[2]) {
+							result.result = '正常';
 							alert(preText + '考勤正常');
 						}else{
 							alert(preText + '不到下班时间哦');
 						}
 					}
+					
+					this.punchList.push(result);
 					
 					this.punchStatus.date = dateStr;
 					if (type === 'signIn') {
@@ -115,6 +180,16 @@
 					} else {
 						this.punchStatus.pm = time;
 					}
+					
+					uni.setStorage({
+						key: 'punchList',
+						data: this.punchList,
+						success: () => {
+							console.log('save punchStatus success');
+							console.log(this.$api, 7777);
+						}
+					});
+					
 					
 					uni.setStorage({
 						key: 'punchStatus',
@@ -133,13 +208,8 @@
 					
 					if (punchStatus) {
 						var date = punchStatus.date;
-						var time = (new Date(date)).getTime();
-						var now = new Date();
-						now = (now.toISOString()).match(/\d{4}-\d{2}-\d{2}/)[0];
-						now = new Date(now);
-						now = now.getTime();
 						//判断是否是当天
-						if (now <= time + 24 * 60 * 60 * 1000) {
+						if (this.checkIsToday(date)) {
 							if (type === 'signIn') {
 								if (punchStatus.am) {
 									alert('您已签到');
